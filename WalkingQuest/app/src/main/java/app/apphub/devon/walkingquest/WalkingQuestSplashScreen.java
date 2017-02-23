7e037f757367d0be279d53b7177bbaa4369e726d
noved210 <devonrimmington@gmail.com>
2017-02-23 12:47:45 -0400

    Root Commit

---
+++ WalkingQuest/app/src/main/java/app/apphub/devon/walkingquest/WalkingQuestSplashScreen.java
@@ -0,0 +1,13 @@
+package app.apphub.devon.walkingquest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WalkingQuestSplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_walking_quest_splash_screen);
+    }
+}
