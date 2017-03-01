f70a19691518aa171585358f274e37b3c9333470
Jonathan <jon_mcdevitt@hotmail.com>
2017-03-01 13:43:00 -0400

    Adding the QuestSelectorGrid Created a QuestSelectorGrid view and the adapter for the quest selector grid object. Additionally, created XML sheets to structure the user interface elements. At its core, a basic UI addition.

---
+++ WalkingQuest/app/src/main/java/app/apphub/devon/walkingquest/QuestSelectorGridActivity.java
@@ -0,0 +1,34 @@
+package app.apphub.devon.walkingquest;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.GridView;
+
+import app.apphub.devon.walkingquest.GridAdapters.QuestSelectorGridAdapter;
+
+public class QuestSelectorGridActivity extends AppCompatActivity {
+    public GridView questGrid;
+    public String[] quests;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quest_selector_grid);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        quests = new String[8];
+        for (int i = 0; i < 8; i++) {
+            quests[i] = (i + 1) + "";
+        }
+
+        questGrid = (GridView) this.findViewById(R.id.quest_selector_grid_view);
+        QuestSelectorGridAdapter questAdapter = new QuestSelectorGridAdapter(QuestSelectorGridActivity.this, quests);
+        questGrid.setAdapter(questAdapter);
+    }
+
+}
