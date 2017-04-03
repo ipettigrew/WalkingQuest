8d5c0c0704760d40d76f877a282544391d3b6129
metzlera <adrianmetzler1@gmail.com>
2017-04-03 11:24:35 -0300

    Main menu screen is now the main screen. Populating Equipment list from database

--- WalkingQuest/app/src/main/java/app/apphub/devon/walkingquest/MainViewActivity.java
+++ WalkingQuest/app/src/main/java/app/apphub/devon/walkingquest/MainViewActivity.java
@@ -2,17 +2,49 @@ package app.apphub.devon.walkingquest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import app.apphub.devon.walkingquest.database.DatabaseHandler;
+import app.apphub.devon.walkingquest.database.objects.Character;
+import app.apphub.devon.walkingquest.database.objects.Inventory;
+import app.apphub.devon.walkingquest.database.objects.Item;
 
 public class MainViewActivity extends AppCompatActivity {
 
+    private DatabaseHandler databaseHandler;
+    private Inventory inventory;
+    private Item item1, item2, item3;
+    private Character character;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_view);
 
-        //ListView listView = (ListView) findViewById(R.id.equipment_list);
-        
+        //initialize the databaseHandler
+        databaseHandler = DatabaseHandler.getInstance(getApplicationContext());
+
+        //just for testing purposes
+        character = new Character("Bob");
+        databaseHandler.addCharacter(character);
+
+        inventory = new Inventory(character.getId());
+        databaseHandler.addInventory(inventory);
+
+        item1 = new Item("test item", 301, inventory.getId());
+        databaseHandler.addItem(item1);
+
+        item2 = new Item("test item2", 302, inventory.getId());
+        databaseHandler.addItem(item2);
+
+        item3 = new Item("test item3", 303, inventory.getId());
+        databaseHandler.addItem(item3);
+
+        ListView listView = (ListView) findViewById(R.id.equipment_list);
+        ArrayAdapter<Item> adapter =
+                new ArrayAdapter<Item>(this, android.R.layout.simple_list_item_1, databaseHandler.getItemsByInventoryId(inventory.getId()));
+        listView.setAdapter(adapter);
+        databaseHandler.close();
     }
 
 
